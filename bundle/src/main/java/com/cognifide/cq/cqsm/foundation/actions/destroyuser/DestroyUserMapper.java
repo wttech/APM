31dd8a7b3d00180863c8f7432869a334e4a050e0
Piotr Wilczyński <piotr.wilczynski@cognifide.com>
2016-09-30 13:32:29 +0200

    updated DestroyUser description

--- bundle/src/main/java/com/cognifide/cq/cqsm/foundation/actions/destroyuser/DestroyUserMapper.java
+++ bundle/src/main/java/com/cognifide/cq/cqsm/foundation/actions/destroyuser/DestroyUserMapper.java
@@ -27,9 +27,7 @@ import com.cognifide.cq.cqsm.api.exceptions.ActionCreationException;
 public class DestroyUserMapper extends BasicActionMapper {
 
 	public static final String REFERENCE = "Remove specified users.\n"
-			+ "Removed user are no longer listed as any group members.\n"
-			+ "Note that no permissions for removed users are cleaned, so after creating a new user with the same id"
-			+ " - it will automatically gain those permissions.";
+			+ "Remove user from assigned groups, all given permission and user itself.";
 
 	@Mapping(
 			value = {"DESTROY" + DASH + "USER" + SPACE + STRING},
