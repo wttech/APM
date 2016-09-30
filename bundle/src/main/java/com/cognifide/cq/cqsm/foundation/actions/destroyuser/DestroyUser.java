fd558f21c663ba3db7092e7794f9749b4bb3cb8e
Piotr Wilczyński <piotr.wilczynski@cognifide.com>
2016-09-30 08:15:36 +0200

    added action to remove from all groups

--- bundle/src/main/java/com/cognifide/cq/cqsm/foundation/actions/destroyuser/DestroyUser.java
+++ bundle/src/main/java/com/cognifide/cq/cqsm/foundation/actions/destroyuser/DestroyUser.java
@@ -1,9 +1,13 @@
 package com.cognifide.cq.cqsm.foundation.actions.destroyuser;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.jcr.RepositoryException;
 
+import org.apache.jackrabbit.api.security.user.Group;
 import org.apache.jackrabbit.api.security.user.User;
 
 import com.cognifide.cq.cqsm.api.actions.Action;
@@ -14,6 +18,7 @@ import com.cognifide.cq.cqsm.api.utils.AuthorizablesUtils;
 import com.cognifide.cq.cqsm.core.utils.MessagingUtils;
 import com.cognifide.cq.cqsm.foundation.actions.CompositeActionResult;
 import com.cognifide.cq.cqsm.foundation.actions.purge.Purge;
+import com.cognifide.cq.cqsm.foundation.actions.removefromgroup.RemoveFromGroup;
 import com.cognifide.cq.cqsm.foundation.actions.removeuser.RemoveUser;
 
 public class DestroyUser implements Action {
@@ -36,9 +41,11 @@ public class DestroyUser implements Action {
 		try {
 			User user = AuthorizablesUtils.getUser(context, userId);
 			context.setCurrentAuthorizable(user);
+			Action removeFromGroups = new RemoveFromGroup(getGroups(user));
 			ActionResult purgeResult = purge.simulate(context);
+			ActionResult removeFromGroupsResult = removeFromGroups.execute(context);
 			ActionResult removeResult = remove.simulate(context);
-			actionResult = new CompositeActionResult(purgeResult, removeResult);
+			actionResult = new CompositeActionResult(purgeResult, removeFromGroupsResult, removeResult);
 		} catch (RepositoryException | ActionExecutionException e) {
 			actionResult = new ActionResult();
 			actionResult.logError(MessagingUtils.createMessage(e));
@@ -52,9 +59,11 @@ public class DestroyUser implements Action {
 		try {
 			User user = AuthorizablesUtils.getUser(context, userId);
 			context.setCurrentAuthorizable(user);
+			Action removeFromGroups = new RemoveFromGroup(getGroups(user));
 			ActionResult purgeResult = purge.execute(context);
+			ActionResult removeFromGroupsResult = removeFromGroups.execute(context);
 			ActionResult removeResult = remove.execute(context);
-			actionResult = new CompositeActionResult(purgeResult, removeResult);
+			actionResult = new CompositeActionResult(purgeResult, removeFromGroupsResult, removeResult);
 		} catch (RepositoryException | ActionExecutionException e) {
 			actionResult = new ActionResult();
 			actionResult.logError(MessagingUtils.createMessage(e));
@@ -65,5 +74,15 @@ public class DestroyUser implements Action {
 	@Override
 	public boolean isGeneric() {
 		return true;
+	}
+
+	private List<String> getGroups(User user) throws RepositoryException {
+		List<String> groups = new ArrayList<>();
+		Iterator<Group> groupIterator = user.declaredMemberOf();
+		while (groupIterator.hasNext()) {
+			Group group = groupIterator.next();
+			groups.add(group.getID());
+		}
+		return groups;
 	}
 }
