a705989d200e1f52e01e7f8b3352051f44bb4fae
Marcin Jędraszczyk <marcin.jedraszczyk@cognifide.com>
2019-03-27 13:09:39 +0100

    Added dashboard.

---
+++ bundle/src/main/java/com/cognifide/cq/cqsm/core/models/DashboardTileModel.java
@@ -0,0 +1,42 @@
+/*
+ * ========================LICENSE_START=================================
+ * AEM Permission Management
+ * %%
+ * Copyright (C) 2013 Cognifide Limited
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =========================LICENSE_END==================================
+ */
+package com.cognifide.cq.cqsm.core.models;
+
+import javax.inject.Inject;
+import lombok.Getter;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+
+@Model(adaptables = Resource.class)
+public class DashboardTileModel {
+
+  @Getter
+  @Inject
+  private String icon;
+
+  @Getter
+  @Inject
+  private String path;
+
+  @Getter
+  @Inject
+  private String title;
+
+}
