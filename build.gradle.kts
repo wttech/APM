65f9c7dc842cf9385f45a2dfeb5b1ae688bd6245
Marcin Jędraszczyk <marcin.jedraszczyk@cognifide.com>
2019-04-08 15:10:38 +0200

    Added apache rat plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    id("org.nosphere.apache.rat") version "0.4.0"
     id("com.cognifide.aem.package")
 }
 
@@ -19,6 +20,46 @@ aem {
             fromProject(":content")
         }
     }
+}
+
+tasks.rat {
+    // Files that don't require a license header
+    excludes.add("**/LICENSE")
+    excludes.add(".travis.yml")
+
+
+    excludes.add("**/clientlibs/externals/ace/js/**")
+    excludes.add("**/clientlibs/js/json2.js")
+
+    excludes.add("**/*.cqsm")
+    excludes.add("**/*.jar")
+    excludes.add("**/*.json")
+    excludes.add("**/*.md")
+    excludes.add("**/*.tld")
+    excludes.add("**/*.txt")
+    excludes.add("**/*.vltignore")
+    excludes.add("**/*.war")
+    excludes.add("**/*.zip")
+    excludes.add("**/*.xml")
+
+    // Module
+    excludes.add("**/documentation/*")
+    excludes.add("**/misc/**")
+
+    // Gradle files
+    excludes.add("**/gradle/**")
+    excludes.add("**/*.gradle.kts")
+    excludes.add("gradlew")
+    excludes.add("gradlew.bat")
+
+    // Eclipse files
+    excludes.add("**/.project")
+    excludes.add("**/.classpath")
+
+    // IntelliJ files
+    excludes.add("**/build/**")
+    excludes.add("**/.idea/**")
+    excludes.add("**/*.iml")
 }
 
 apply(from = "gradle/common.gradle.kts")
--- bundle/src/main/java/com/cognifide/cq/cqsm/core/jobs/JobResultsCache.java
+++ bundle/src/main/java/com/cognifide/cq/cqsm/core/jobs/JobResultsCache.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,38 +23,36 @@ import com.cognifide.cq.cqsm.api.logger.Progress;
 import com.cognifide.cq.cqsm.core.Property;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
-
+import java.util.concurrent.TimeUnit;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 
-import java.util.concurrent.TimeUnit;
-
 @Component(
-		immediate = true,
-		service = JobResultsCache.class,
-		property = {
-				Property.DESCRIPTION + "Job results holder service",
-				Property.VENDOR
-		}
+    immediate = true,
+    service = JobResultsCache.class,
+    property = {
+        Property.DESCRIPTION + "Job results holder service",
+        Property.VENDOR
+    }
 )
 public class JobResultsCache {
 
-	private static final long DEFAULT_EXPIRATION_TIME = 10;
+  private static final long DEFAULT_EXPIRATION_TIME = 10;
 
-	private Cache<String, Progress> cache;
+  private Cache<String, Progress> cache;
 
-	@Activate
-	public void activate(final ComponentContext componentContext) {
-		cache = CacheBuilder.newBuilder().expireAfterWrite(DEFAULT_EXPIRATION_TIME, TimeUnit.MINUTES).build();
-	}
+  @Activate
+  public void activate(final ComponentContext componentContext) {
+    cache = CacheBuilder.newBuilder().expireAfterWrite(DEFAULT_EXPIRATION_TIME, TimeUnit.MINUTES).build();
+  }
 
-	public void put(String id, Progress progress) {
-		cache.put(id, progress);
-	}
+  public void put(String id, Progress progress) {
+    cache.put(id, progress);
+  }
 
-	public Progress get(String id) {
-		return cache.getIfPresent(id);
-	}
+  public Progress get(String id) {
+    return cache.getIfPresent(id);
+  }
 
 }
