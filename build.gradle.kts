d726398cb7ba97c2d8ae313873f4f9a6abd2aa0b
Maciej Geisler <maciej.geisler@cognifide.com>
2020-03-04 16:39:44 +0100

    Reverting local gradle props

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,15 +4,12 @@ import pl.allegro.tech.build.axion.release.domain.scm.ScmPosition
 
 plugins {
     id("pl.allegro.tech.build.axion-release") version "1.10.1"
-    id("org.nosphere.apache.rat") version "0.4.0"
-    id("com.cognifide.aem.bundle")
     id("com.cognifide.aem.instance")
     id("com.cognifide.aem.package")
     `maven-publish`
     signing
 }
 
-group = "com.company.aem"
 defaultTasks = listOf(":aemSatisfy", ":aemDeploy")
 description = "AEM Permission Management :: Root"
 
@@ -64,47 +61,6 @@ publishing {
             }
         }
     }
-}
-
-tasks.rat {
-    // Files that don't require a license header
-    excludes.add("**/LICENSE")
-    excludes.add(".travis.yml")
-
-    excludes.add("**/main/generated/**")
-    excludes.add("**/clientlibs/externals/ace/js/**")
-    excludes.add("**/clientlibs/js/json2.js")
-
-    excludes.add("**/*.cqsm")
-    excludes.add("**/*.jar")
-    excludes.add("**/*.json")
-    excludes.add("**/*.md")
-    excludes.add("**/*.tld")
-    excludes.add("**/*.txt")
-    excludes.add("**/*.vltignore")
-    excludes.add("**/*.war")
-    excludes.add("**/*.zip")
-    excludes.add("**/*.xml")
-
-    // Module
-    excludes.add("**/documentation/*")
-    excludes.add("**/misc/**")
-
-    // Gradle files
-    excludes.add("**/gradle/**")
-    excludes.add("**/*.gradle.kts")
-    excludes.add("gradlew")
-    excludes.add("gradlew.bat")
-
-    // Eclipse files
-    excludes.add("**/.project")
-    excludes.add("**/.classpath")
-
-    // IntelliJ files
-    excludes.add("**/build/**")
-    excludes.add("**/out/**")
-    excludes.add("**/.idea/**")
-    excludes.add("**/*.iml")
 }
 
 apply(from = "gradle/common.gradle.kts")
