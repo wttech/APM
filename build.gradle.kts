2dc15ee35458a4950dfe05af9ca081c47e67c000
Marcin Jędraszczyk <marcin.jedraszczyk@cognifide.com>
2021-01-08 13:22:24 +0100

    Excluded node_modules from license checking.

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,6 +77,9 @@ tasks {
 
         // Swagger API Definition
         excludes.add("**/apidefinition/*.yaml")
+
+        // Node dependencies
+        excludes.add("**/node_modules/**")
     }
 }
 
