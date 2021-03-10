e4769d6780a174c3050751efc8c59ebb48dd52ca
Dominik Przybyl <przybyl.dominik@gmail.com>
2021-03-10 21:18:41 +0100

    reordered imports

--- app/aem/core/src/main/java/com/cognifide/apm/core/launchers/StartupScriptLauncher.java
+++ app/aem/core/src/main/java/com/cognifide/apm/core/launchers/StartupScriptLauncher.java
@@ -21,8 +21,12 @@ package com.cognifide.apm.core.launchers;
 
 import static com.cognifide.apm.core.scripts.ScriptFilters.onStartup;
 
+import com.cognifide.apm.api.scripts.Script;
+import com.cognifide.apm.api.services.ScriptFinder;
+import com.cognifide.apm.api.services.ScriptManager;
+import com.cognifide.apm.core.Property;
+import com.cognifide.apm.core.utils.sling.SlingHelper;
 import java.util.List;
-
 import org.apache.sling.api.resource.PersistenceException;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
@@ -30,12 +34,6 @@ import org.apache.sling.settings.SlingSettingsService;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-
-import com.cognifide.apm.api.scripts.Script;
-import com.cognifide.apm.api.services.ScriptFinder;
-import com.cognifide.apm.api.services.ScriptManager;
-import com.cognifide.apm.core.Property;
-import com.cognifide.apm.core.utils.sling.SlingHelper;
 
 @Component(
     immediate = true,
