51a275eab68a49a2684e0c17d832d4f55da841be
Dominik Przybyl <przybyl.dominik@gmail.com>
2022-06-02 12:36:45 +0200

    refactor

--- app/aem/ui.apps.cloud/build.gradle.kts
+++ app/aem/ui.apps.cloud/build.gradle.kts
@@ -21,19 +21,11 @@ aem {
     tasks {
         packageCompose {
             mergePackageProject(":app:aem:ui.apps.base")
-            installBundleProject(":app:aem:api") {
-                startLevel.set(20)
-            }
-            installBundleProject(":app:aem:core") {
-                startLevel.set(23)
-            }
-            installBundleProject(":app:aem:actions.main") {
-                startLevel.set(21)
-            }
+            installBundleProject(":app:aem:api")
+            installBundleProject(":app:aem:core")
+            installBundleProject(":app:aem:actions.main")
+            installBundleProject(":app:aem:simple")
             installBundleProject(":app:aem:startup") {
-                startLevel.set(27)
-            }
-            installBundleProject(":app:aem:simple") {
                 startLevel.set(27)
             }
             vaultDefinition {
