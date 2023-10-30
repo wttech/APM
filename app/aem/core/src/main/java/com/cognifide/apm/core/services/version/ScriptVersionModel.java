d2822178a04846636bb5c9803d160ceacd663bf9
Dominik Przybyl <przybyl.dominik@gmail.com>
2023-10-30 20:59:18 +0100

    fixed large history log issue

--- app/aem/core/src/main/java/com/cognifide/apm/core/services/version/ScriptVersionModel.java
+++ app/aem/core/src/main/java/com/cognifide/apm/core/services/version/ScriptVersionModel.java
@@ -21,9 +21,10 @@ package com.cognifide.apm.core.services.version;
 
 import javax.inject.Inject;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 
-@Model(adaptables = Resource.class)
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class ScriptVersionModel implements ScriptVersion {
 
   @Inject
